8867688d6dbb4a772436665da932810fc9f72f40
sergiodev19 <sergio.dev19@gmail.com>
2021-10-18 02:34:19 +0400

    .

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -9,7 +9,7 @@ nvm install node
 # Create your working directory if it does not exist
 
 DIR="/home/ec2-user/my-app-one"
-if [ ! -d "$DIR" ]; then
+if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else 
   echo "Creating ${DIR} directory"
