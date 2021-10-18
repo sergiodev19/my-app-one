1d67498c4f7996122443c66efc5b737a1684abb7
sergiodev19 <sergio.dev19@gmail.com>
2021-10-19 00:14:53 +0400

    -

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+
+#give permission for everything in the my-app-one directory
 sudo chmod -R 777 /home/ec2-user/my-app-one
 
-# Navigate into our orking directory here we have all our github files
+#navigate into our working directory where we have all our github files
 cd /home/ec2-user/my-app-one
 
-# Add npm and node to path
-export NVM_DIR="$HOME/.nvm"
-[ -s "NVM_DIR/nvm.sh" ] && \. "NVM_DIR/nvm.sh" # loads nvm
-[ -s "NVM_DIR/bash_completion" ] && \. "NVM_DIR/bash_completion"
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-# install node modules
+#install node modules
 npm install
 
-# start our node app in the background
-node app.js >  app.out.log 2> app.err.log < /dev/numm &
+#start our node app in the background
+node app.js > app.out.log 2> app.err.log < /dev/null & 
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Download node and npm
-
+#download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 
-# Create your working directory if it does not exist
-
+#create our working directory if it doesnt exist
 DIR="/home/ec2-user/my-app-one"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
-else 
+else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
 fi
